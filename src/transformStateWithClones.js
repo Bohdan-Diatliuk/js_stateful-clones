9cfc16f69a0ebbee400b1072058d8c420cfb964e
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2025-09-10 19:47:54 +0300

    replaced if in switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,29 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
   let prevState = { ...state };
   const result = [];
 
   for (const action of actions) {
     let newState = { ...prevState };
 
-    if (action.type === 'addProperties') {
-      Object.assign(newState, action.extraData);
-      result.push(newState);
-    }
-
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete newState[key];
+    switch (action.type) {
+      case 'addProperties': {
+        Object.assign(newState, action.extraData);
+        result.push(newState);
+        break;
       }
-      result.push(newState);
+
+      case 'removeProperties': {
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        result.push(newState);
+
+        break;
+      }
+      case 'clear':
+        newState = {};
+
+        result.push(newState);
     }
 
-    if (action.type === 'clear') {
-      newState = {};
-      result.push(newState);
-    }
     prevState = newState;
   }
 
